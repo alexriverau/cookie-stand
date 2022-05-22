a5490303bb4edd59d61a773af2473df08b57d8b7
Alejandro Rivera <103381515+alexriverau@users.noreply.github.com>
2022-05-21 23:15:56 -0500

    Revert "Class06 objects"

--- js/app.js
+++ js/app.js
@@ -1,29 +1,29 @@
 'use strict';
 
-// SALES DATA TO DO
-// min hourly customers per location
-// max hourly customers per location
-// avg cookies per customer an hour at each location
+function randCustomer(min, max) {
+  return Math.floor(min + Math.random() * (max - min));
+}
+console.log(randCustomer(23, 65));
 
 let storeHours = [
-  '6am:',
-  '7am:',
-  '8am:',
-  '9am:',
-  '10am:',
-  '11am:',
-  '12pm:',
-  '1pm:',
-  '2pm:',
-  '3pm:',
-  '4pm:',
-  '5pm:',
-  '6pm:',
-  '7pm:',
-  '8pm:',
+  '6am',
+  '7am',
+  '8am',
+  '9am',
+  '10am',
+  '11am',
+  '12pm',
+  '1pm',
+  '2pm',
+  '3pm',
+  '4pm',
+  '5pm',
+  '6pm',
+  '7pm',
+  '8pm',
 ];
 
-let storeLocations = [
+let salesLocations = [
   {
     location: 'Seattle',
     minCust: 23,
@@ -56,44 +56,17 @@ let storeLocations = [
   },
 ];
 
-function randNumber(min, max) {
-  return Math.floor(min + Math.random() * (max - min));
-}
-// console.log(randNumber(23, 65));
-
-function randNumberCust(location) {
-  let randHourCust = `${randNumber(location.minCust, location.maxCust)}`;
-  return randHourCust;
-}
-// randNumberCust();
-
-// TEST MATH.RANDOM
-// for (let i = 0; i < storeHours.length; i++) {
-//   let cookiesSold = `${storeHours[i]} ${randNumberCust(storeLocations[0])} cookies`;
-//   console.log(cookiesSold);
-// }
-// TEST LOCATIONS DISPLAY
-// for (let i = 0; i < storeLocations.length; i++) {
-//   let storeLocation = storeLocations[i];
-//   let locationSales = `${storeLocation.location}`;
-//   console.log(locationSales);
-// }
-
-function locationSales(location) {
-  for (let i = 0; i < storeLocations.length; i++) {
-    let storeLocation = storeLocations[i];
-    let locationCity = `${storeLocation.location}`;
-    console.log(locationCity);
-    for (let i = 0; i < storeHours.length; i++) {
-      let storeHour = storeHours[i];
-      let storeSales = `${storeHour} ${randNumberCust(location)} cookies`;
-      console.log(storeSales);
-    }
+function cookieSales() {
+  for (let i = 0; i < salesLocations.length; i++) {
+    let salesLocation = salesLocations[i];
   }
 }
-locationSales(storeLocations[0]);
 
-// let salesSeattle = document.createElement('li');
-// salesSeattle.innerText = `${storeHours}`;
-// console.log(salesSeattle);
-// document.getElementById('seattle').appendChild(salesSeattle);
+// for (let i = 0; s < storeHours.length; i++) {
+//   let storeHour = storeHours[i];
+// }
+
+let salesSeattle = document.createElement('li');
+salesSeattle.innerText = `${storeHours}`;
+console.log(salesSeattle);
+document.getElementById('seattle').appendChild(salesSeattle);
