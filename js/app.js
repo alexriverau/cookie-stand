0b79e9d3c75eb7819c708ffcdcfc1783e7d1ca64
Alejandro Rivera <alexrivera78@gmail.com>
2022-05-31 17:49:22 -0500

    added totals completed lab06

--- js/app.js
+++ js/app.js
@@ -35,54 +35,66 @@ let storeLocations = [
     minCust: 23,
     maxCust: 65,
     avgCookies: 6.3,
+    totalCookies: 0,
   },
   {
     location: 'Tokyo',
     minCust: 3,
     maxCust: 24,
     avgCookies: 1.2,
+    totalCookies: 0,
   },
   {
     location: 'Dubai',
     minCust: 11,
     maxCust: 38,
     avgCookies: 3.7,
+    totalCookies: 0,
   },
   {
     location: 'Paris',
     minCust: 20,
     maxCust: 38,
     avgCookies: 2.3,
+    totalCookies: 0,
   },
   {
     location: 'Lima',
     minCust: 2,
     maxCust: 16,
     avgCookies: 4.6,
+    totalCookies: 0,
   },
 ];
 
 function locationSales(location) {
   for (let i = 0; i < storeLocations.length; i++) {
     let storeLocation = storeLocations[i];
-    let cityLi = document.createElement('li');
-    cityLi.innerText = `${storeLocation.location}`;
-    document.getElementById('city').appendChild(cityLi);
-    for (let i = 0; i < storeHours.length; i++) {
-      let storeHour = storeHours[i];
+
+    let cityUl = document.createElement('ul');
+    cityUl.innerText = `${storeLocation.location}`;
+    document.getElementById('city').appendChild(cityUl);
+
+    for (let j = 0; j < storeHours.length; j++) {
+      let storeHour = storeHours[j];
       // console.log(storeHour);
       let randCustomer = Math.round(
         location.minCust + Math.random() * (location.maxCust - location.minCust)
       );
       // console.log(randCustomer);
       let randAvg = Math.round(randCustomer * location.avgCookies);
+      storeLocation.totalCookies += randAvg;
       // console.log(randAvg);
       let storeSales = `${storeHour} ${randAvg} cookies`;
       // console.log(storeSales);
+
       let salesLi = document.createElement('li');
       salesLi.innerText = `${storeSales}`;
       document.getElementById('city').appendChild(salesLi);
     }
+    let locationTotal = document.createElement('li');
+    locationTotal.innerText = `Total: ${storeLocation.totalCookies} cookies`;
+    document.getElementById('city').appendChild(locationTotal);
   }
 }
 locationSales(storeLocations[0]);
